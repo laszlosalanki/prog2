750d7c013f51e92a0907e0662df29ea9f1c08910
laszlosalanki <55296085+laszlosalanki@users.noreply.github.com>
2020-09-20 15:58:27 +0200

    Add files via upload

---
+++ Gagyi2.java
@@ -0,0 +1,15 @@
+public class Gagyi2
+{
+	public static void main (String[] args)
+	{
+		Integer x = -129;
+		Integer t = -129;
+
+		System.out.println(x);
+		System.out.println(t);
+
+		while (x <= t && x >= t && t != x) {
+
+		}
+	}
+}
