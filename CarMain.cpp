2f414f46fb37a5c8978d6db7726843617dbe3d32
laszlosalanki <55296085+laszlosalanki@users.noreply.github.com>
2020-09-26 15:31:17 +0200

    Add files via upload

---
+++ CarMain.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class CarParent {
+    public:
+        string rendszam;
+        string gyarto;
+        string tipus;
+
+        CarParent(string rndszm, string gyrt, string tps) {
+            rendszam = rndszm;
+            gyarto = gyrt;
+            tipus = tps;
+        }
+
+        string getRendszam() {
+            return rendszam;
+        }
+};
+
+class CarChild : public CarParent{
+    public:
+        CarChild(string rndszm, string gyrt, string tps) : CarParent(rndszm, gyrt, tps) {
+            rendszam = rndszm;
+            gyarto = gyrt;
+            tipus = tps;
+        }
+
+        string getGyarto() {
+            return rendszam;
+        }
+};
+
+int main() {
+
+    CarParent* cp = new CarChild("ABC-123", "Ford", "Fiesta");
+    CarChild* cc = new CarChild("ZYX-321", "Opel", "Astra");
+
+    cout << cc->getGyarto() << " " << cp->getGyarto() << endl;
+
+    return 0;
+}
